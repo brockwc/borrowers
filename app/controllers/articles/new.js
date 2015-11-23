0f3e5d4c21f210ba737ce7322f13ef9d0e4b12bd
zap-rowsdower <brock.whitbread@gmail.com>
2015-11-23 15:23:52 -0500

    built a controller on my own, biiiiiiiiiitch

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,24 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.description'));
+      }
+    }
+  ),
+  actions: {
+    save() {
+      if (this.get('isValid')) {
+        return true;
+      } else {
+        this.set('errorMessage', 'You have to fill in a description');
+      }
+    },
+    cancel() {
+      return true;
+    }
+  }
+});
