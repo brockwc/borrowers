04f53a5ce2b27da5e779f91ff751b86e1105e1d9
zap-rowsdower <brock.whitbread@gmail.com>
2015-11-24 19:33:24 -0500

    need to specify the host to avoid XML http request error

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-    namespace: 'api/v2'
+  host: 'http://api.ember-cli-101.com',
+  namespace: 'api/v2'
 });
